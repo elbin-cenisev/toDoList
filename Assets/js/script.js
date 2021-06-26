aec8ca0ad4d7ae0662496c5587053ed4af9df93d
Elbin Cenisev <elbincenisev@outlook.com>
2021-06-26 19:04:19 -0400

    Overdue items now show red

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,35 @@
-// Add moment.js date (just day and date)
+// Get today's date and display it in currentDay <p>
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+console.log($("tr:contains('10')").css("background", "red"));
+
+function checkOverdue() {
+    // Get the current time for comparisons
+    var currentTime = moment().format('HH');
+
+    // Loop through all local storage items
+    for (var i = 0; i < localStorage.length; i++){
+
+        // Create variable holding the first two numbers
+        let taskTime = (JSON.parse(localStorage.key(i))).substring(0,2);
+        console.log(taskTime);
+
+        // If currentTime > first two numbers (hour) of entry
+        if(currentTime > taskTime) {
+            
+            // find the correct row and turn it red
+            $("tr:contains(" + taskTime + ")").css("background", "red")
+
+        }
+
+        // If currentTime < first two numbers of entry
+            // Turn the fields green
+
+        // If currentTime = first two numbers of entry
+            // Turn the fields green
+    }
+}
 
 
 // EventListener for save button in each row
@@ -19,3 +48,5 @@ $("#timeTable").on("click", '.saveButton', function() {
     localStorage.setItem(JSON.stringify(time), JSON.stringify(userInput));
 
 })
+
+checkOverdue();
